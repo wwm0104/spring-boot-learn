de1859e892ceb9a8ba2a71f67c1747fd83714139
wanmin.wei <>
2020-03-26 23:00:56 +0800

    多种启动方式

--- spring-boot-service/src/main/java/com/dragon/myapplication/Application.java
+++ spring-boot-service/src/main/java/com/dragon/myapplication/Application.java
@@ -1,5 +1,6 @@
 package com.dragon.myapplication;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
+        /*
+        //定制启动
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setBannerMode(Banner.Mode.CONSOLE);
+        springApplication.run(args);*/
         SpringApplication.run(Application.class,args);
     }
 }
